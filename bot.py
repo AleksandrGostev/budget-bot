269cba379a4addc25444e68d006b090e54c382d8
aleksandr.gostev <aleksandr.gostev@solita.fi>
2019-09-26 16:34:18 +0300

    fully switch to postgres

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from db_service import PaymentType
 import calendar
 import os
 
-TOKEN = os.environ.get("TOKEN", None)
+TOKEN = os.environ.get('TOKEN')
 
 
 # only used for console output now
@@ -26,14 +26,16 @@ bot.set_update_listener(listener)  # register listener
 chart_with_upwards_trend = u'\U0001F4C8'
 chart_with_downwards_trend = u'\U0001F4C9'
 
+db_service = db_service.BotDB()
+
 
 class MessageHandler:
-    insert_expense_category = False
-    insert_income_category = False
+    insert_category = False
     rename_category = False
     category_id = None
     price = None
     title = None
+    category_type = None
 
 
 def main_menu():
@@ -55,22 +57,23 @@ def show_report(message):
     incomes_str = ""
     expense_str = ""
 
-    for category_row in category_rows:
-        cat = dict(category_row)
-        if cat['payment_type'] == 'expense':
-            expense_str += "{}: {} €\n".format(cat['title'], cat['amount'])
+    for cat in category_rows:
+        title = cat[3]
+        amount = cat[6]
+        payment_type = cat[2]
+        if payment_type == 'expense':
+            expense_str += "{}: {} €\n".format(title, amount)
         else:
-            incomes_str += "{}: {} €\n".format(cat['title'], cat['amount'])
+            incomes_str += "{}: {} €\n".format(title, amount)
 
-    full_msg = "Report for {} - {}:\n\nIncomes:\n{}\nExpenses:\n{}".format(first_day_of_month, last_day_of_month,
-                                                                           incomes_str, expense_str)
+    full_msg = "Отчёт по дате {} - {}:\n\nДоходы:\n{}\nРасходы:\n{}".format(first_day_of_month, last_day_of_month,
+                                                                            incomes_str, expense_str)
 
     bot.send_message(message.chat.id, full_msg)
 
 
 @bot.message_handler(commands=['setup'])
 def setup(message):
-    # markup = types.ReplyKeyboardMarkup(row_width=3, resize_keyboard=True, one_time_keyboard=True)
     markup = main_menu()
     bot.send_message(message.chat.id, "Чё хочешь хозяин?", reply_markup=markup)
 
@@ -78,18 +81,18 @@ def setup(message):
 @bot.callback_query_handler(func=lambda call: call.data == 'expense' or call.data == 'income')
 def categories_menu(callback):
     markup = types.InlineKeyboardMarkup(row_width=1)
-    # get list of categories
-    rows = db_service.get_categories(callback.message.chat.id, callback.data)
-    for row in rows:
-        cat = dict(row)
+    categories = db_service.get_categories(callback.message.chat.id, callback.data)
+    for cat in categories:
+        category_id = cat[0]
+        title = cat[3]
         markup.add(
-            types.InlineKeyboardButton(cat['title'], callback_data="category_edit_{}".format(cat['category_id'])))
-    add_category_btn = types.InlineKeyboardButton("Add new category",
+            types.InlineKeyboardButton(title, callback_data="category_edit_{}".format(category_id)))
+    add_category_btn = types.InlineKeyboardButton("Добавить категорию",
                                                   callback_data='add_category_{}'.format(callback.data))
 
-    back_category_btn = types.InlineKeyboardButton("Back", callback_data='back_to_main_menu')
+    back_category_btn = types.InlineKeyboardButton("Назад", callback_data='back_to_main_menu')
     markup.add(add_category_btn, back_category_btn)
-    bot.edit_message_text("Please select category",
+    bot.edit_message_text("Выбери категорию",
                           callback.message.chat.id, callback.message.message_id, reply_markup=markup)
 
 
@@ -97,11 +100,9 @@ def categories_menu(callback):
 def add_category(callback):
     callback_data_arr = callback.data.split('_')
     category = callback_data_arr[len(callback_data_arr) - 1]
-    bot.send_message(callback.message.chat.id, "Please write down category to add it to {}".format(category))
-    if category == "expense":
-        MessageHandler.insert_expense_category = True
-    if category == "income":
-        MessageHandler.insert_income_category = True
+    bot.send_message(callback.message.chat.id, "Напиши название категории")
+    MessageHandler.insert_category = True
+    MessageHandler.category_type = category
 
 
 @bot.callback_query_handler(func=lambda callback: callback.data == 'back_to_main_menu')
@@ -118,15 +119,16 @@ def start_message(message):
 @bot.callback_query_handler(func=lambda call: call.data.startswith('category_edit_'))
 def category_edit_menu(callback):
     category_id = callback.data.split('_')[-1]
-    category_row = db_service.get_category(callback.message.chat.id, category_id)
-    category = dict(category_row)
+    cat = db_service.get_category(callback.message.chat.id, category_id)
+    title = cat[3]
+    payment_type = cat[2]
     markup = types.InlineKeyboardMarkup(row_width=2)
 
-    markup.add(types.InlineKeyboardButton("Rename", callback_data='rename_category_{}'.format(category_id)),
-               types.InlineKeyboardButton("Delete", callback_data='delete_category_{}'.format(category_id)))
-    markup.add(types.InlineKeyboardButton("Back", callback_data=category['payment_type']))
+    markup.add(types.InlineKeyboardButton("Переименовать", callback_data='rename_category_{}'.format(category_id)),
+               types.InlineKeyboardButton("Удалить", callback_data='delete_category_{}'.format(category_id)))
+    markup.add(types.InlineKeyboardButton("Назад", callback_data=payment_type))
 
-    bot.edit_message_text("Soon here will be sum for this category:\n {}".format(category['title']),
+    bot.edit_message_text("За текущий месяц:\n {} - 0 €".format(title),
                           callback.message.chat.id, callback.message.message_id)
     bot.edit_message_reply_markup(callback.message.chat.id, callback.message.message_id, reply_markup=markup)
 
@@ -136,7 +138,7 @@ def rename_category(callback):
     category_id = callback.data.split('_')[-1]
     MessageHandler.rename_category = True
     MessageHandler.category_id = category_id
-    bot.send_message(callback.message.chat.id, "Please enter category new name")
+    bot.send_message(callback.message.chat.id, "Введи новое имя категории")
 
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith('delete_category'))
@@ -144,19 +146,17 @@ def delete_category(callback):
     markup = types.InlineKeyboardMarkup()
     category_id = callback.data.split('_')[-1]
     markup.add(
-        types.InlineKeyboardButton("Yes", callback_data="proceed_{}".format(callback.data)),
-        types.InlineKeyboardButton("No", callback_data="category_edit_{}".format(category_id))
+        types.InlineKeyboardButton("Да", callback_data="proceed_{}".format(callback.data)),
+        types.InlineKeyboardButton("Нет", callback_data="category_edit_{}".format(category_id))
     )
-    bot.edit_message_text("Are you sure?", callback.message.chat.id, callback.message.message_id, reply_markup=markup)
+    bot.edit_message_text("Ты уверен?", callback.message.chat.id, callback.message.message_id, reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith('proceed_delete_category'))
 def proceed_delete_category(callback):
     category_id = callback.data.split('_')[-1]
-    MessageHandler.rename_category = True
-    MessageHandler.rename_category_id = category_id
-    db_service.delete_category(callback.message.chat.id, MessageHandler.category_id)
-    bot.send_message(callback.message.chat.id, "Category deleted")
+    db_service.delete_category(callback.message.chat.id, category_id)
+    bot.send_message(callback.message.chat.id, "Категория удалена")
 
 
 @bot.callback_query_handler(func=lambda call: call.data == 'payment_categories_main_menu')
@@ -173,7 +173,7 @@ def get_payment_menu(callback):
     payment_type = callback.data.split('_')[-1]
     markup = types.InlineKeyboardMarkup(row_width=2)
     generate_add_menu(markup, payment_type, callback.message.chat.id)
-    markup.add(types.InlineKeyboardButton("Back", callback_data="payment_categories_main_menu"))
+    markup.add(types.InlineKeyboardButton("Назад", callback_data="payment_categories_main_menu"))
     bot.edit_message_reply_markup(callback.message.chat.id, callback.message.message_id, reply_markup=markup)
 
 
@@ -182,7 +182,7 @@ def cancel_payment(callback):
     payment_id = callback.data.split("_")[-1]
     db_service.delete_payment(callback.message.chat.id, payment_id)
     cancelled_icon = u'\U0000274C'
-    bot.edit_message_text(callback.message.text + "\n {} cancelled".format(cancelled_icon), callback.message.chat.id,
+    bot.edit_message_text(callback.message.text + "\n {} отменено".format(cancelled_icon), callback.message.chat.id,
                           callback.message.message_id, reply_markup=())
 
 
@@ -190,50 +190,46 @@ def cancel_payment(callback):
 def add_payment(callback):
     category_id = callback.data.split("_")[-1]
     payment_type = callback.data.split("_")[-2]
-    category_row = db_service.get_category(callback.message.chat.id, category_id)
-    category = dict(category_row)
+    cat = db_service.get_category(callback.message.chat.id, category_id)
+    title = cat[3]
     payment_id = db_service.insert_payment(callback.message.chat.id, callback.message.from_user.first_name,
                                            payment_type,
                                            MessageHandler.title, MessageHandler.price, datetime.datetime.today(),
                                            category_id)
     markup = types.InlineKeyboardMarkup(row_width=2)
-    markup.add(types.InlineKeyboardButton("Cancel", callback_data="cancel_payment_{}".format(payment_id)))
-    bot.edit_message_text("{} was added to {}".format(MessageHandler.price, category['title']),
+    markup.add(types.InlineKeyboardButton("Отменить", callback_data="cancel_payment_{}".format(payment_id)))
+    bot.edit_message_text("{} добавлено в {}".format(MessageHandler.price, title),
                           callback.message.chat.id, callback.message.message_id, reply_markup=markup)
 
 
 def generate_add_menu(markup, payment_type, chat_id):
-    rows = db_service.get_categories(chat_id, payment_type)
-    for row in rows:
-        category = dict(row)
+    categories = db_service.get_categories(chat_id, payment_type)
+    for cat in categories:
+        category_id = cat[0]
+        title = cat[3]
         markup.add(
             types.InlineKeyboardButton(
-                category['title'],
-                callback_data="add_payment_{}_{}".format(payment_type, category['category_id'])
+                title,
+                callback_data="add_payment_{}_{}".format(payment_type, category_id)
             )
         )
 
 
 @bot.message_handler(func=lambda message: True)
 def all_messages_handler(message):
-    if MessageHandler.insert_expense_category:
-        bot.send_message(message.chat.id, "Expense category added:\n {} ".format(message.text))
+    if MessageHandler.insert_category:
+        bot.send_message(message.chat.id, "Категория добавлена:\n {} ".format(message.text))
         categories_list = message.text.split('\n')
         for category in categories_list:
-            db_service.insert_category(message.chat.id, PaymentType.EXPENSE, category, datetime.datetime.now(), 1)
-        MessageHandler.insert_expense_category = False
-        return
-
-    if MessageHandler.insert_income_category:
-        bot.send_message(message.chat.id, "Income category soon will be added: {}".format(message.text))
-        db_service.insert_category(message.chat.id, PaymentType.INCOME, message.text, datetime.datetime.now(), 1)
-        MessageHandler.insert_income_category = False
+            db_service.insert_category(
+                message.chat.id, MessageHandler.category_type, category, datetime.datetime.now())
+        MessageHandler.insert_category = False
         return
 
     if MessageHandler.rename_category:
         db_service.rename_category(message.chat.id, MessageHandler.category_id, message.text)
         MessageHandler.rename_category = False
-        bot.send_message(message.chat.id, "Category name is updated")
+        bot.send_message(message.chat.id, "Имя категории обновлено")
         return
 
     markup = types.InlineKeyboardMarkup(row_width=2)
@@ -247,8 +243,7 @@ def all_messages_handler(message):
             bot.send_message(message.chat.id, "Я всё мужу расскажу!!")
         else:
             bot.send_message(message.chat.id, "Красава Санёк!!")
-        bot.send_message(message.chat.id, "Where add?", reply_markup=markup)
-        # bot.send_message(message.chat.id, "Soon categories will be shown for:\n {} \n ${}$".format(price, desc))
+        bot.send_message(message.chat.id, "Куда желаете добавить?", reply_markup=markup)
     if "-" not in message.text:
         try:
             price = int(message.text)
@@ -258,8 +253,7 @@ def all_messages_handler(message):
                 bot.send_message(message.chat.id, "Я всё мужу расскажу!!")
             else:
                 bot.send_message(message.chat.id, "Красава Санёк!!")
-            bot.send_message(message.chat.id, "Where add?", reply_markup=markup)
-            # bot.send_message(message.chat.id, "Soon categories will be shown for:\n {}".format(price))
+            bot.send_message(message.chat.id, "Извольте выбрать категорию?", reply_markup=markup)
         except ValueError:
             bot.send_sticker(message.chat.id, "CAADAgADogADNmLjBTODpXLHHo4DFgQ")
 
--- db_service.py
+++ db_service.py
@@ -1,6 +1,7 @@
-import sqlite3
 import datetime
 import calendar
+import psycopg2
+import os
 
 
 class PaymentType:
@@ -8,117 +9,79 @@ class PaymentType:
     INCOME = 'income'
 
 
-# conn = sqlite3.connect('db_budget.db')
-#
-# cursor = conn.cursor()
+class BotDB:
+    connection = None
+    cursor = None
 
+    def __init__(self):
+        self.connection = psycopg2.connect(user=os.environ.get('pg-user'),
+                                           password=os.environ.get('pg-password'),
+                                           host="balarama.db.elephantsql.com",
+                                           port="5432",
+                                           database=os.environ.get('pg-user'))
+        self.cursor = self.connection.cursor()
 
-# cursor.execute("""CREATE TABLE payments (
-#                         payment_id integer primary key,
-#                         chat_id integer,
-#                         username text,
-#                         title text,
-#                         price real,
-#                         date text,
-#                         category_id integer
-#                 )""")
-#
-# cursor.execute("""CREATE TABLE categories (
-#                         category_id integer primary key,
-#                         chat_id integer,
-#                         payment_type text,
-#                         title text,
-#                         date text,
-#                         position integer
-#                 )""")
+    def __del__(self):
+        self.connection.close()
 
+    def insert_payment(self, chat_id, username, payment_type, title, price, date, category_id):
+        self.cursor.execute(
+            """INSERT INTO payments(chat_id, username, title, price, date, category_id) 
+                VALUES ( %s, %s, %s, %s, %s, %s )""",
+            (chat_id, username, title, price, date, category_id))
+        self.connection.commit()
+        return self.cursor.lastrowid
 
-def insert_payment(chat_id, username, payment_type, title, price, date, category_id):
-    conn = sqlite3.connect('db_budget.db')
-    cursor = conn.cursor()
-    cursor.execute(
-        """INSERT INTO payments('chat_id', 'username', 'title', 'price', 'date', 'category_id') 
-            VALUES ( ?, ?, ?, ?, ?, ? )""",
-        (chat_id, username, title, price, date, category_id))
-    conn.commit()
-    return cursor.lastrowid
+    def insert_category(self, chat_id, payment_type, title, date):
+        self.cursor.execute(
+            """INSERT INTO categories (chat_id, payment_type, title, date, position)
+                VALUES ( %s, %s, %s, %s, (SELECT COALESCE(COUNT(*)+1, 0) as category_position FROM categories WHERE chat_id = %s))""",
+            (chat_id, payment_type, title, date, chat_id))
+        self.connection.commit()
 
+    def get_categories(self, chat_id, payment_type):
+        self.cursor.execute("SELECT * FROM categories WHERE chat_id = %s and payment_type = %s",
+                            (chat_id, payment_type))
+        self.connection.commit()
+        return self.cursor.fetchall()
 
-def insert_category(chat_id, payment_type, title, date, position):
-    conn = sqlite3.connect('db_budget.db')
-    cursor = conn.cursor()
-    cursor.execute("""INSERT INTO categories ("chat_id", "payment_type", "title", "date", "position")
-                    VALUES ( ?, ?, ?, ?, ? )""", (chat_id, payment_type, title, date, position))
-    conn.commit()
+    def get_category(self, chat_id, category_id):
+        self.cursor.execute("SELECT * FROM categories WHERE chat_id = %s and category_id = %s", (chat_id, category_id))
+        self.connection.commit()
+        return self.cursor.fetchone()
 
+    def rename_category(self, chat_id, category_id, title):
+        self.cursor.execute("UPDATE categories SET title = %s WHERE chat_id = %s and category_id = %s",
+                            (title, chat_id, category_id))
+        self.connection.commit()
 
-def get_categories(chat_id, payment_type):
-    conn = sqlite3.connect('db_budget.db')
-    conn.row_factory = sqlite3.Row
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM categories WHERE chat_id = ? and payment_type = ?", (chat_id, payment_type))
-    conn.commit()
-    return cursor.fetchall()
+    def delete_category(self, chat_id, category_id):
+        self.cursor.execute("DELETE FROM payments WHERE chat_id = %s and category_id = %s ", (chat_id, category_id))
+        self.cursor.execute("DELETE FROM categories WHERE chat_id = %s and category_id = %s", (chat_id, category_id))
+        self.connection.commit()
 
+    def delete_payment(self, chat_id, payment_id):
+        self.cursor.execute("DELETE FROM payments WHERE chat_id = %s and payment_id = %s ", (chat_id, payment_id))
+        self.connection.commit()
 
-def get_category(chat_id, category_id):
-    conn = sqlite3.connect('db_budget.db')
-    conn.row_factory = sqlite3.Row
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM categories WHERE chat_id = ? and category_id = ?", (chat_id, category_id))
-    conn.commit()
-    return cursor.fetchone()
-
-
-def rename_category(chat_id, category_id, title):
-    conn = sqlite3.connect('db_budget.db')
-    cursor = conn.cursor()
-    cursor.execute("UPDATE categories SET title = ? WHERE chat_id = ? and category_id = ?",
-                   (title, chat_id, category_id))
-    conn.commit()
-
-
-def delete_category(chat_id, category_id):
-    conn = sqlite3.connect('db_budget.db')
-    cursor = conn.cursor()
-    cursor.execute("DELETE FROM payments WHERE chat_id = ? and category_id = ? ", (chat_id, category_id))
-    cursor.execute("DELETE FROM categories WHERE chat_id = ? and category_id = ?", (chat_id, category_id))
-    conn.commit()
-
-
-def delete_payment(chat_id, payment_id):
-    conn = sqlite3.connect('db_budget.db')
-    cursor = conn.cursor()
-    cursor.execute("DELETE FROM payments WHERE chat_id = ? and payment_id = ? ", (chat_id, payment_id))
-    conn.commit()
-
-
-def get_category_payments_current_month(chat_id, category_id):
-    conn = sqlite3.connect('db_budget.db')
-    conn.row_factory = sqlite3.Row
-    cursor = conn.cursor()
-    # first_day_of_month = datetime.datetime.today().replace(day=1)
-    month_range = calendar.monthrange(datetime.date.year, datetime.date.month)
-    first_day_of_month = month_range[0]
-    last_day_of_month = month_range[1]
-    cursor.execute("""SELECT c.*, sum(p.price) as amount
-                        FROM categories as c
-                        LEFT JOIN payments as p on p.category_id = c.category_id
-                        WHERE c.chat_id = ? and date >= ? and c.date <= ?
-                        group by c.category_id""",
-                   (chat_id, first_day_of_month, last_day_of_month))
-    conn.commit()
-    return cursor.fetchall()
-
-
-def get_chat_payments_current_month(chat_id, first_day_of_month, last_day_of_month):
-    conn = sqlite3.connect('db_budget.db')
-    conn.row_factory = sqlite3.Row
-    cursor = conn.cursor()
-    cursor.execute("""SELECT c.*, IFNULL(SUM(p.price), 0) amount
+    def get_category_payments_current_month(self, chat_id, category_id):
+        month_range = calendar.monthrange(datetime.date.year, datetime.date.month)
+        first_day_of_month = month_range[0]
+        last_day_of_month = month_range[1]
+        self.cursor.execute("""SELECT c.*, sum(p.price) as amount
                             FROM categories as c
-                            LEFT JOIN payments as p on p.category_id = c.category_id 
-                                AND c.chat_id = ? and p.date >= ? and p.date <= ?
+                            LEFT JOIN payments as p on p.category_id = c.category_id
+                            WHERE c.chat_id = %s and date >= %s and c.date <= %s
                             group by c.category_id""",
-                   (chat_id, first_day_of_month, last_day_of_month))
-    return cursor.fetchall()
+                            (chat_id, first_day_of_month, last_day_of_month))
+        self.connection.commit()
+        return self.cursor.fetchall()
+
+    def get_chat_payments_current_month(self, chat_id, first_day_of_month, last_day_of_month):
+        self.cursor.execute("""SELECT c.*, COALESCE (SUM(p.price), 0) amount
+                                FROM categories as c
+                                LEFT JOIN payments as p on p.category_id = c.category_id 
+                                    AND c.chat_id = %s and p.date >= %s and p.date <= %s
+                                group by c.category_id""",
+                            (chat_id, str(first_day_of_month), str(last_day_of_month)))
+        return self.cursor.fetchall()

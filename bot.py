6d2bf3c853d7b4d79a8c26e312f155ee7baabe4c
aleksandr.gostev <aleksandr.gostev@solita.fi>
2019-09-26 16:43:19 +0300

    update to webhook

--- bot.py
+++ bot.py
@@ -5,8 +5,10 @@ import db_service
 from db_service import PaymentType
 import calendar
 import os
+from flask import Flask, request
 
 TOKEN = os.environ.get('TOKEN')
+server = Flask(__name__)
 
 
 # only used for console output now
@@ -258,4 +260,18 @@ def all_messages_handler(message):
             bot.send_sticker(message.chat.id, "CAADAgADogADNmLjBTODpXLHHo4DFgQ")
 
 
-bot.polling()
+@server.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://telegram-gos-budget.herokuapp.com/' + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
